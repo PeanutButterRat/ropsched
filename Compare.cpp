72731012d1945c8ee3d96c6598d63e4152c917df
Eric Brown <ebrown5676@gmail.com>
2025-10-25 12:37:02 -0700

    Add data move instruction category

--- Compare.cpp
+++ Compare.cpp
@@ -5,29 +5,47 @@
 using namespace llvm;
 
 
+constexpr std::array<StringLiteral, 6> DataMovePrefixes { "PUSH", "POP", "MOV", "XCHG", "LEA", "CMOV" };
 
 
 struct Compare {
   const TargetInstrInfo *TII;
 
+  enum InstrCategory {
+    DataMove,
+    Other,
+  };
+
   explicit Compare(const TargetInstrInfo *TII = nullptr) : TII(TII) {}
 
-  bool operator() (SUnit *A, SUnit *B) const {
-    return getInstructionScore(A) <= getInstructionScore(B);
+  bool operator() (SUnit *IA, SUnit *IB) const {
+    return getInstrScore(IA) <= getInstrScore(IB);
   }
 
-  float getInstructionScore(SUnit *SU) const {
+  float getInstrScore(SUnit *SU) const {
     assert(TII && "TII is null");
     assert(SU && "SUnit is null");
 
     MachineInstr *MI = SU->getInstr();
     unsigned int Opcode = MI->getOpcode();
     StringRef Name = TII->getName(Opcode);
+    InstrCategory Category = getInstrCategory(Name);
 
-    if (Name.starts_with("ADD")) {
-      return 1.0f;
+    switch (Category) {
+      case DataMove:
+        return 1.0f;
+      default:
+        return 0.0f;
+    }
+  }
+
+  InstrCategory getInstrCategory(const StringRef Name) const {
+    for (auto Prefix : DataMovePrefixes) {
+      if (Name.starts_with(Prefix)) {
+        return DataMove;
+      }
     }
 
-    return 0.0f;
+    return Other;
   }
 };
